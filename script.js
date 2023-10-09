b3665984659759ebbe552973e3447a04b6b42838
Dave Jornales <plusultrastral@gmail.com>
2023-10-10 07:43:40 +0800

    card initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// FLIP EFFECT ===========================================
+var front = document.querySelector(".jinx-box-front");
+var back = document.querySelector(".jinx-box-back");
+var jinxBox = document.querySelector("#jinx-box");
+var heimmerBox = document.querySelector("#heimmer-box");
+
+jinxBox.addEventListener("click", function () {
+  if (jinxBox.classList.contains("flipped")) {
+    jinxBox.classList.remove("flipped");
+  } else {
+    jinxBox.classList.add("flipped");
+  }
+});
+
+heimmerBox.addEventListener("click", function () {
+  if (heimmerBox.classList.contains("flipped")) {
+    heimmerBox.classList.remove("flipped");
+  } else {
+    heimmerBox.classList.add("flipped");
+  }
+});
